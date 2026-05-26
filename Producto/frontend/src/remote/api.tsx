ecc567aedb870e25fac0ff56e262968bda08de3b
Solgrey <sol.medina@duocuc.cl>
2026-05-26 13:46:43 -0400

    se guardan cambios

--- Producto/frontend/src/remote/api.tsx
+++ Producto/frontend/src/remote/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8050/api/v1",
+    baseURL: "http://127.0.0.1:8050/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
